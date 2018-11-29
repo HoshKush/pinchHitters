c96b11fa1a630ef5bdbb35b3b5dad94d342e8e2a
soldesk <soldesk@soldesk-PC>
2018-11-29 15:03:47 +0900

    declaration commit

---
+++ pinchHitters/src/main/java/spring/model/declaration/DeclarationDAO.java
@@ -0,0 +1,52 @@
+package spring.model.declaration;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DeclarationDAO implements IDeclarationDAO {
+ 
+	@Autowired
+	private SqlSessionTemplate mybatis;
+	
+	@Override
+	public boolean create(Object dto) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List list(Map map) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object read(Object pk) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Object dto) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Object pk) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int total(Map map) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ pinchHitters/src/main/java/spring/model/declaration/IDeclarationDAO.java
@@ -0,0 +1,7 @@
+package spring.model.declaration;
+
+import spring.model.hitterface.DAOSTDInter;
+
+public interface IDeclarationDAO extends DAOSTDInter {
+
+}
